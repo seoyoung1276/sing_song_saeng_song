f7f5fb7c1438375be3945424fa8fb8392ac90d10
seoyoung1276 <seoyoung1276@gmail.com>
2023-06-17 17:22:48 +0900

    노래 객체 추가

---
+++ songIndy/indy.js
@@ -0,0 +1,29 @@
+var indy1 = {
+    title: "잔나비 - 주저하는 연인들을 위해",
+    answer: "주저하는 연인들을 위해",
+    secondsong: 'songs/잔나비 - 주저하는 연인들을 위해.mp3',
+    hint:"ㅈㅈㅎㄴㅇㄴㄷㅇㅇㅎ",
+    answersong: 'songs/A잔나비 - 주저하는 연인들을 위해.mp3',
+    answerimage: 'image/1.jpg'
+};
+  
+var indy2 = {
+    title: "백아 - 첫사랑",
+    answer: "첫사랑",
+    secondsong: 'songs/백아 - 첫사랑.mp3',
+    hint: "ㅊㅅㄹ",
+    answersong: 'songs/A백아 - 첫사랑.mp3',
+    answerimage: 'image/2.jpg'
+};
+
+var indy3 = {
+    title: "검정치마 - everything",
+    answer:["everything","에브리띵"],
+    secondsong: 'songs/검정치마 - everything.mp3',
+    hint: "ㅇㅂㄹㄸ",
+    answersong: 'songs/A검정치마 - everything.mp3',
+    answerimage: 'image/3.jpg'
+};
+  
+  // 퀴즈 배열에 객체 추가
+  var indyquiz = [indy1, indy2, indy3];
