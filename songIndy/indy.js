e19ff298a4a62e6603be50a2171e9670c8227a38
seoyoung1276 <seoyoung1276@gmail.com>
2023-06-21 10:26:48 +0900

    다음 문제로 넘어감

--- songIndy/indy.js
+++ songIndy/indy.js
@@ -22,7 +22,7 @@ var indy = [
  {
     "id":3,
     "title": "검정치마 - everything",
-    "answer":["everything","에브리띵", "Everything"],
+    "answer":["everything", "에브리띵", "Everything"],
     "secondsong": 'songs/검정치마 - everything.mp3',
     "hint": "ㅇㅂㄹㄸ",
     "answersong": 'songs/A검정치마 - everything.mp3',
@@ -30,23 +30,95 @@ var indy = [
  }
 ];
 
+var inputanswer = document.getElementById("inputanswer");
+var answer ="";
 
-    var firstTitle = indy[0].title;
-    var musicTitle = document.getElementById("musicTitle");
+const cover = document.getElementById("coverbox");
+const songbox = document.getElementById("songbox");
+const hint = document.getElementById("hint");
+const musicTitle = document.getElementById("musicTitle");
 
-    if (musicTitle) {   
-        musicTitle.textContent = firstTitle;
+cover.style.display = 'none';
+musicTitle.style.display = 'none';
+
+var bol = false;
+var i = 0; 
+
+function nextquiz() {
+    if(bol === false){
+        inputanswer.value = null;
+        inputanswer.style.display = 'block';
+        cover.style.display = 'none';
+        musicTitle.style.display = 'none';
+        songbox.style.display = 'block';
+        hint.style.display = 'block';
+        //hint.textContent = indy[i].hint;
     }
+}
 
-
-    if (typeof nextBtn !== 'undefined' && nextBtn !== null) {
-        nextBtn.addEventListener("click", function(){
-            window.location.href = 'Answer.html';
-        }); 
+// 엔터 키 동작
+function handleEnterKey(event) {
+    if(event.key === 'Enter'){
+        input();
     }
+}
 
-    if (typeof toGameBtn !== 'undefined' && toGameBtn !== null) {
-        toGameBtn.addEventListener("click", function(){
-            window.location.href = 'secondGame.html';
-        });
+inputanswer.addEventListener('keydown', handleEnterKey)
+
+function input() {
+    answer = inputanswer.value;
+    checkanswer();
+}
+
+function checkanswer() {
+    for(var j=0; j<indy[i].answer.length; j++){
+        if(answer === indy[i].answer[j]){
+            bol = true; 
+            cover.style.display = 'block';
+            inputanswer.style.display = 'none';
+            musicTitle.style.display = 'block';
+            cover.src = indy[i].answerimage;
+            musicTitle.textContent = indy[i].title;
+            songbox.style.display = 'none';
+            hint.style.display = 'none';
+            alert(answer);
+            i++;
+            break; 
+        }else{
+            alert("땡!");
+            break;
+        }
     }
+}
+
+// 다음 문제 버튼 클릭시
+if (typeof nextBtn !== 'undefined' && nextBtn !== null) {
+    nextBtn.addEventListener("click", function(){
+        if(i < indy.length) {
+            bol = false;
+            nextquiz();
+        }else{
+            alert("문제 끝~ ^>^");
+        }
+    }); 
+}
+
+    // let i = 0;
+    // let b = false;
+
+    // var firstTitle = indy[i].title;
+    // var musicTitle = document.getElementById("musicTitle");
+
+    // if (musicTitle) {   
+    //     musicTitle.textContent = firstTitle;
+    // }
+
+
+    
+
+    // if (typeof toGameBtn !== 'undefined' && toGameBtn !== null) {
+    //     toGameBtn.addEventListener("click", function(){
+    //         i++;
+    //         window.location.href = 'secondGame.html';
+    //     });
+    // }
